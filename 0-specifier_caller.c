fd60cc3c1f91cb46b6f1d50254928ea5b99b975a
humade <hayfordkojo18@gmail.com>
2023-08-24 09:29:05 +0000

    More base files

---
+++ 0-specifier_caller.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * spec_pe_call - Function calls a spec_function
+ * @a: % input
+ * @b: Other character input such as 'c', 's', etc
+ *
+ * Description: Uses an array to check which function to call
+ * based on specifier 'a''b'.
+ * Return: A pointer to the required function.
+ */
+int (*spec_pe_call(char *a, char *b))(char *, int, va_list)
+{
+	func_t option[] = {
+		{"%", "c", spec_c},
+		{"%", "s", spec_s},
+		{"%", "%", spec_pe},
+		{NULL, NULL, NULL}
+	};
+
+	int i = 0;
+
+	while ((option[i].a) != NULL && *(option[i].a) != *a && *(option[i].b) != *b)
+		i++;
+
+	if (option[i].a == NULL)
+		return (NULL);
+
+	return (option[i].f);
+}
--- main.h
+++ main.h
@@ -11,5 +11,21 @@ int spec_s(char *str, int index, va_list argx);
 int spec_pe(char *str, int index, va_list argx);
 /* Specifier %d function */
 int spec_d(char *str, int index, va_list argx);
+/* A function Pointer for % family*/
+int (*spec_pe_call(char *a, char *b))(char *, int, va_list);
+
+/**
+ * struct func - Structure for specifier & it's call function
+ *
+ * @a: First character to check for specifier
+ * @b: Second character to check for specifier
+ * @f: Function associated with 'st' and 'nd'
+ */
+typedef struct func
+{
+	char *a;
+	char *b;
+	int (*f)(char *s, int a, va_list ap);
+} func_t;
 
 #endif
